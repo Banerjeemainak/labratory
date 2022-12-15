57c718c7f335a898b2aa3c2c480c4274dc6b45ed
Mainak Banerjee <120621021+Banerjeemainak@users.noreply.github.com>
2022-12-15 13:31:57 +0530

    Create Factorial.java

---
+++ Assignment 15-12-2022/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,no,f=1;
+        System.out.println("Enter the number: ");
+        no=sc.nextInt();
+        for(i=1;i<=no;i++)
+        {
+            f=f*i;
+        }
+        System.out.println("Factorial is:"+f);
+    }    
+}
