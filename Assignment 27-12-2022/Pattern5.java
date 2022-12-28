79b85c192f394b8808fb5c2f89770768a5a892f1
Mainak Banerjee <120621021+Banerjeemainak@users.noreply.github.com>
2022-12-28 16:02:07 +0530

    Create Pattern5.java

---
+++ Assignment 27-12-2022/Pattern5.java
@@ -0,0 +1,29 @@
+    *
+   ***
+  *****
+ *******
+*********
+
+package basic;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		int k=0;
+		for(int i=1;i<=5;i++,k=0)
+		{
+			for(int j=1;j<=5-i;j++)
+			{
+				System.out.print(" ");
+			}
+			while(k!=2*i-1)
+			{
+				System.out.print("*");
+				k++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
